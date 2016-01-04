241a04473bac240f4c1e023cef9e13b9c00e34bd
1nicks52@gmail.com <nikhita@Nikhitas-MacBook-Pro.local>
2016-01-04 18:07:50 +0530

    Appium project norm- Nexus 5 API 21-Android studio :wq :

---
+++ Appium1/app/src/main/java/com/example/nikhita/appiumpro1/BrowseAppActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.nikhita.appiumpro1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class BrowseAppActivityFragment extends Fragment {
+
+    public BrowseAppActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_browse_app, container, false);
+    }
+}
